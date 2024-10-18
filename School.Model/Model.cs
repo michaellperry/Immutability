1314bf9dc575937ad480f0fe52a6b1ed9489abbb
Michael L Perry <michael@qedcode.com>
2024-10-18 17:09:26 -0500

    Export the school model to code

---
+++ School.Model/Model.cs
@@ -0,0 +1,15 @@
+﻿namespace School.Model;
+
+using Jinaga;
+
+[FactType("School.Institution")]
+public record Institution(string identifier) {}
+
+[FactType("School.Course")]
+public record Course(Institution institution, string code) {}
+
+[FactType("School.Course.Title")]
+public record CourseTitle(Course course, string title, CourseTitle[] prior) {}
+
+[FactType("School.Course.Credits")]
+public record CourseCredits(Course course, int credits, CourseCredits[] prior) {}
